31ef2318177fb351448f7c59eee9371412da2a59
Adrian Smith <triode1@btinternet.com>
2014-07-05 23:45:06 +0100

    add checksums for install scripts, add update-all script

---
+++ soa-update-all.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ "$1" == "-f" ]; then
+    opt="-f"
+else
+    opt="--needed"
+fi
+
+# PKGBUILD files using git source get modified - checkout again
+git pull
+git checkout HEAD -- $(git ls-files -m)
+
+cd turbolua
+makepkg --asroot -c -i -s --noconfirm "$opt"
+cd ..
+
+cd soa-web
+makepkg --asroot -c -i -s --noconfirm "$opt"
+cd ..
+
+cd libsoxr
+makepkg --asroot -c -i -s --noconfirm "$opt"
+cd ..
+
+cd squeezelite
+makepkg --asroot -c -i -s --noconfirm "$opt"
+cd ..
+
+cd jivelite
+makepkg --asroot -c -i -s --noconfirm "$opt"
+cd ..
+
+cd jivelite-autologin
+makepkg --asroot -c -i -s --noconfirm "$opt"
+cd ..
+
+if [ `pacman -Qq logitechmediaserver` == "logitechmediaserver" ]; then
+    cd logitechmediaserver-7.8
+    makepkg --asroot -c -i -s --noconfirm "$opt"
+    cd ..
+fi
+
+if [ `pacman -Qq logitechmediaserver-cpan` == "logitechmediaserver-cpan" ]; then
+    cd logitechmediaserver-7.9-cpan
+    makepkg --asroot -c -i -s --noconfirm "$opt"
+    cd ..
+fi
+
+if [ `pacman -Qq logitechmediaserver-lms` == "logitechmediaserver-lms" ]; then
+    cd logitechmediaserver-7.9-lms
+    makepkg --asroot -c -i -s --noconfirm "$opt"
+    cd ..
+fi
--- soa-update.sh
+++ soa-update.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+if [ "$1" == "-f" ]; then
+	force=$1
+	shift
+fi
+
 app=$1
 
 if [ ! -d "$app" ]; then
-    echo "Usage soa-update <directory>"
+    echo "Usage soa-update [-f] <directory>"
     exit
 fi
 
 # PKGBUILD files using git source get modified - checkout again
-git checkout jivelite/PKGBUILD
-git checkout soa-web/PKGBUILD
-git checkout squeezelite/PKGBUILD
-
 git pull
+git checkout HEAD -- $(git ls-files -m)
 
 cd $app
-makepkg --asroot -c -i -f -s --noconfirm
+makepkg --asroot -c -i "$force" -s --noconfirm
 cd ..
 
