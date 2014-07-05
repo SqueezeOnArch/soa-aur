e0c8cf0e6ffd633010684200efe37af8196288c1
Adrian Smith <triode1@btinternet.com>
2014-07-06 00:08:31 +0100

    fix lms build version, don't show progressbar on update

--- soa-update-all.sh
+++ soa-update-all.sh
@@ -11,43 +11,43 @@ git pull
 git checkout HEAD -- $(git ls-files -m)
 
 cd turbolua
-makepkg --asroot -c -i -s --noconfirm "$opt"
+makepkg --asroot -c -i -s --noconfirm --noprogressbar "$opt"
 cd ..
 
 cd soa-web
-makepkg --asroot -c -i -s --noconfirm "$opt"
+makepkg --asroot -c -i -s --noconfirm --noprogressbar "$opt"
 cd ..
 
 cd libsoxr
-makepkg --asroot -c -i -s --noconfirm "$opt"
+makepkg --asroot -c -i -s --noconfirm --noprogressbar "$opt"
 cd ..
 
 cd squeezelite
-makepkg --asroot -c -i -s --noconfirm "$opt"
+makepkg --asroot -c -i -s --noconfirm --noprogressbar "$opt"
 cd ..
 
 cd jivelite
-makepkg --asroot -c -i -s --noconfirm "$opt"
+makepkg --asroot -c -i -s --noconfirm --noprogressbar "$opt"
 cd ..
 
 cd jivelite-autologin
-makepkg --asroot -c -i -s --noconfirm "$opt"
+makepkg --asroot -c -i -s --noconfirm --noprogressbar "$opt"
 cd ..
 
 if [[ `pacman -Qq logitechmediaserver` == "logitechmediaserver" ]]; then
     cd logitechmediaserver-7.8
-    makepkg --asroot -c -i -s --noconfirm "$opt"
+    makepkg --asroot -c -i -s --noconfirm --noprogressbar "$opt"
     cd ..
 fi
 
 if [[ `pacman -Qq logitechmediaserver-cpan` == "logitechmediaserver-cpan" ]]; then
     cd logitechmediaserver-7.9-cpan
-    makepkg --asroot -c -i -s --noconfirm "$opt"
+    makepkg --asroot -c -i -s --noconfirm --noprogressbar "$opt"
     cd ..
 fi
 
 if [[ `pacman -Qq logitechmediaserver-lms` == "logitechmediaserver-lms" ]]; then
     cd logitechmediaserver-7.9-lms
-    makepkg --asroot -c -i -s --noconfirm "$opt"
+    makepkg --asroot -c -i -s --noconfirm --noprogressbar "$opt"
     cd ..
 fi
