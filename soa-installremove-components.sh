65e23734ee6f5fee22466a6538589c3e7f8cb2c7
Adrian Smith <triode1@btinternet.com>
2014-07-09 21:46:01 +0100

    install remove script

---
+++ soa-installremove-components.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+if [[ "$#" == "0" ]]; then
+    echo "Usage: soa-installremove-componets.sh <install|remove> <component1> <install|remove> <component2>"
+    exit 1
+fi
+
+echo Install / Remove Components started: `date`
+echo 
+
+pushd `dirname $0` > /dev/null
+
+# PKGBUILD files using git source get modified - checkout again
+echo sychronising scripts
+git pull
+git checkout HEAD -- $(git ls-files -m)
+
+while (( "$#" )); do
+
+    action=$1
+    component=$2
+    shift 2
+    
+    if [[ "$component" == "squeezelite" ]]; then
+	if [[ "$action" == "remove" ]]; then
+	    pacman -R --noconfirm squeezelite libsoxr
+	fi
+	if [[ "$action" == "install" ]]; then
+	    cd libsoxr
+	    makepkg --asroot -c -i -f -s --noconfirm
+	    cd ..
+	    cd squeezelite
+	    makepkg --asroot -c -i -f -s --noconfirm
+	    cd ..
+	fi
+    fi
+
+    if [[ "$component" == "jivelite" ]]; then
+	if [[ "$action" == "remove" ]]; then
+	    pacman -R --noconfirm jivelite-autologin jivelite
+	fi
+	if [[ "$action" == "install" ]]; then
+	    cd jivelite
+	    makepkg --asroot -c -i -f -s --noconfirm
+	    cd ..
+	    cd jivelite-autologin
+	    makepkg --asroot -c -i -f -s --noconfirm
+	    cd ..
+	fi
+    fi
+
+    if [[ "$component" == "server78" ]]; then
+	if [[ "$action" == "remove" ]]; then
+	    pacman -R --noconfirm logitechmediaserver
+	fi
+	if [[ "$action" == "install" ]]; then
+	    cd logitechmediaserver-7.8
+	    makepkg --asroot -c -i -s --noconfirm
+	    cd ..
+	fi
+    fi
+
+    if [[ "$component" == "server79" ]]; then
+	if [[ "$action" == "remove" ]]; then
+	    pacman -R --noconfirm logitechmediaserver-lms logitechmediaserver-cpan
+	fi
+	if [[ "$action" == "install" ]]; then
+	    cd logitechmediaserver-7.9-cpan
+	    makepkg --asroot -c -i -s --noconfirm
+	    cd ..
+	    cd logitechmediaserver-7.9-lms
+	    makepkg --asroot -c -i -s --noconfirm
+	    cd ..
+	fi
+    fi
+
+done
+
+popd > /dev/null
+
+echo
+echo Install / Remove Components complete: `date`
