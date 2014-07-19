88af6cb5fc39f7e6f1c18d8852c22159fbf4c2ac
Adrian Smith <triode1@btinternet.com>
2014-07-19 16:53:33 +0100

    add wandboard kernel to install remove

--- soa-installremove-components.sh
+++ soa-installremove-components.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ "$#" == "0" ]]; then
-    echo "Usage: soa-installremove-componets.sh <install|remove> <component1> <install|remove> <component2>"
-    exit 1
+	echo "Usage: soa-installremove-componets.sh <install|remove> <component1> <install|remove> <component2>"
+	exit 1
 fi
 
 echo Install / Remove Components started: `date`
@@ -17,62 +17,74 @@ git pull
 
 while (( "$#" )); do
 
-    action=$1
-    component=$2
-    shift 2
+	action=$1
+	component=$2
+	shift 2
     
-    if [[ "$component" == "squeezelite" ]]; then
-	if [[ "$action" == "remove" ]]; then
-	    pacman -R --noconfirm squeezelite libsoxr
+	if [[ "$component" == "squeezelite" ]]; then
+		if [[ "$action" == "remove" ]]; then
+			pacman -R --noconfirm squeezelite libsoxr
+		fi
+		if [[ "$action" == "install" ]]; then
+			cd libsoxr
+			makepkg --asroot -c -i -f -s --noconfirm
+			cd ..
+			cd squeezelite
+			makepkg --asroot -c -i -f -s --noconfirm
+			cd ..
+		fi
 	fi
-	if [[ "$action" == "install" ]]; then
-	    cd libsoxr
-	    makepkg --asroot -c -i -f -s --noconfirm
-	    cd ..
-	    cd squeezelite
-	    makepkg --asroot -c -i -f -s --noconfirm
-	    cd ..
+	
+	if [[ "$component" == "jivelite" ]]; then
+		if [[ "$action" == "remove" ]]; then
+			pacman -R --noconfirm jivelite-autologin jivelite
+		fi
+		if [[ "$action" == "install" ]]; then
+			cd jivelite
+			makepkg --asroot -c -i -f -s --noconfirm
+			cd ..
+			cd jivelite-autologin
+			makepkg --asroot -c -i -f -s --noconfirm
+			cd ..
+		fi
 	fi
-    fi
-
-    if [[ "$component" == "jivelite" ]]; then
-	if [[ "$action" == "remove" ]]; then
-	    pacman -R --noconfirm jivelite-autologin jivelite
+	
+	if [[ "$component" == "server78" ]]; then
+		if [[ "$action" == "remove" ]]; then
+			pacman -R --noconfirm logitechmediaserver
+		fi
+		if [[ "$action" == "install" ]]; then
+			cd logitechmediaserver-7.8
+			makepkg --asroot -c -i -s --noconfirm
+			cd ..
+		fi
 	fi
-	if [[ "$action" == "install" ]]; then
-	    cd jivelite
-	    makepkg --asroot -c -i -f -s --noconfirm
-	    cd ..
-	    cd jivelite-autologin
-	    makepkg --asroot -c -i -f -s --noconfirm
-	    cd ..
+	
+	if [[ "$component" == "server79" ]]; then
+		if [[ "$action" == "remove" ]]; then
+			pacman -R --noconfirm logitechmediaserver-lms logitechmediaserver-cpan
+		fi
+		if [[ "$action" == "install" ]]; then
+			cd logitechmediaserver-7.9-cpan
+			makepkg --asroot -c -i -s --noconfirm
+			cd ..
+			cd logitechmediaserver-7.9-lms
+			makepkg --asroot -c -i -s --noconfirm
+			cd ..
+		fi
 	fi
-    fi
-
-    if [[ "$component" == "server78" ]]; then
-	if [[ "$action" == "remove" ]]; then
-	    pacman -R --noconfirm logitechmediaserver
+	
+	if [[ "$component" == "linux-wandboard-soa" ]]; then
+		if [[ "$action" == "remove" ]]; then
+			pacman -R --noconfirm linux-wandboard-soa
+			pacman -S --noconfigm linux-wandboard
+		fi
+		if [[ "$action" == "install" ]]; then
+			cd linux-wandboard
+			makepkg --asroot -c -i -s --noconfirm
+			cd ..
+		fi
 	fi
-	if [[ "$action" == "install" ]]; then
-	    cd logitechmediaserver-7.8
-	    makepkg --asroot -c -i -s --noconfirm
-	    cd ..
-	fi
-    fi
-
-    if [[ "$component" == "server79" ]]; then
-	if [[ "$action" == "remove" ]]; then
-	    pacman -R --noconfirm logitechmediaserver-lms logitechmediaserver-cpan
-	fi
-	if [[ "$action" == "install" ]]; then
-	    cd logitechmediaserver-7.9-cpan
-	    makepkg --asroot -c -i -s --noconfirm
-	    cd ..
-	    cd logitechmediaserver-7.9-lms
-	    makepkg --asroot -c -i -s --noconfirm
-	    cd ..
-	fi
-    fi
 
 done
 
